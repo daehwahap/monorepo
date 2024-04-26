51b36fab30dd1971a603d247b47d9192d97aacee
11t518s <11t518s20161759@gmail.com>
2024-04-27 01:28:11 +0900

    feat: add ios login

--- apps/app/src/screens/googleLoginTest.tsx
+++ apps/app/src/screens/googleLoginTest.tsx
@@ -1,10 +1,23 @@
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from '@react-native-google-signin/google-signin';
 import React from 'react';
 import {Button, View} from 'react-native';
 
+GoogleSignin.configure({
+  iosClientId:
+    '67810102731-171m3q6smdkn63u9afjqk49l6ql1i1mi.apps.googleusercontent.com',
+});
+
 export const GoogleLoginTest = () => {
-  const handleLoginPress = () => {
-    const OAuthUrl =
-      'https://oauth2.googleapis.com/tokeninfo?id_token=%7BID_TOKEN%7D%22%EC%9C%BC%EB%A1%9C';
+  const handleLoginPress = async () => {
+    //
+    // GoogleSignin.
+    console.log('aaa');
+    GoogleSignin.signIn();
+    console.log('token', await GoogleSignin.getTokens());
+    console.log('currentUser', await GoogleSignin.getCurrentUser());
   };
   return (
     <View
@@ -13,7 +26,12 @@ export const GoogleLoginTest = () => {
         backgroundColor: 'white',
         marginTop: 48,
       }}>
-      <Button title="구글 로그인 버튼" onPress={() => console.log('aa')} />
+      <GoogleSigninButton
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={handleLoginPress}
+      />
+      <Button title="구글 로그인 버튼" onPress={handleLoginPress} />
     </View>
   );
 };

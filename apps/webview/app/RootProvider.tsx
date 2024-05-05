762b4cc42d95434156cd240ca384602785f233d4
yoon-junseo <ysj6844@naver.com>
2024-05-05 23:31:46 +0900

    feat: RootProvider에 ReactQueryProvider 추가

--- apps/webview/app/RootProvider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { PropsWithChildren } from "react";
-import { BridgeProvider } from "../WebPostMessageBridge";
-
-export const RootProvider = (props: PropsWithChildren) => {
-  return <BridgeProvider>{props.children}</BridgeProvider>;
-};

119dc5c5d821930d62c739e8bc61532ce92be62e
yoon-junseo <ysj6844@naver.com>
2024-05-05 23:29:51 +0900

    feat: webview에 trpc 추가

---
+++ apps/webview/src/shared/trpc/index.ts
@@ -0,0 +1,14 @@
+import { AppRouter } from '@pinit/server/src/trpc/trpc.router'
+import { createTRPCProxyClient, createTRPCReact, httpBatchLink } from '@trpc/react-query'
+
+export const trpcOption = {
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:8080/trpc', // you should update this to use env variables
+    }),
+  ],
+}
+
+export const trpcQuery = createTRPCReact<AppRouter>()
+
+export const trpc = createTRPCProxyClient<AppRouter>(trpcOption)

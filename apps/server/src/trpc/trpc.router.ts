20e0b4134ee26f988793503ab5a811ffb2436bc2
yoon-junseo <ysj6844@naver.com>
2024-05-08 22:03:06 +0900

    feat: router에서 하위 router 합치는 방식을 변경
    * 사용처에서 도메인 단위로 타입 추론 가능하도록 변경

--- apps/server/src/trpc/trpc.router.ts
+++ apps/server/src/trpc/trpc.router.ts
@@ -1,30 +1,35 @@
-import { UserController } from './user/user.controller';
-import { INestApplication, Injectable } from '@nestjs/common';
-import * as trpcExpress from '@trpc/server/adapters/express';
-import { TrpcService } from './trpc.service';
+import { UserController } from './user/user.controller'
+import { INestApplication, Injectable } from '@nestjs/common'
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { TrpcService } from './trpc.service'
+import { AwsController } from './aws/aws.controller'
 
 @Injectable()
 export class TrpcRouter {
   constructor(
-    private readonly userController: UserController,
     private readonly trpcService: TrpcService,
+    private readonly userController: UserController,
+    private readonly awsController: AwsController,
   ) {}
 
-  mergeRouter = this.trpcService.mergeRouters(this.userController.router);
+  appRouter = this.trpcService.router({
+    user: this.userController.router,
+    aws: this.awsController.router,
+  })
 
   async applyMiddleware(app: INestApplication) {
     app.use(
       `/trpc`,
       trpcExpress.createExpressMiddleware({
-        router: this.mergeRouter,
+        router: this.appRouter,
         createContext: this.trpcService.createContext as any,
         onError(opts) {
-          const { error, type, path, input, ctx, req } = opts;
-          console.error('Error:', error);
+          const { error, type, path, input, ctx, req } = opts
+          console.error('Error:', error)
         },
       }),
-    );
+    )
   }
 }
 
-export type AppRouter = TrpcRouter['mergeRouter'];
+export type AppRouter = TrpcRouter['appRouter']

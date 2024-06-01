e1db797b8418a8456b2c63569c35614fdb1330d2
yoon-junseo <ysj6844@naver.com>
2024-06-01 15:13:12 +0900

    feat: webview에 import/order 설정

--- apps/webview/.eslintrc.js
+++ apps/webview/.eslintrc.js
@@ -1,8 +1,44 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@pinit/eslint-config/next.js"],
+  extends: ['@pinit/eslint-config/next.js'],
   parserOptions: {
     project: true,
   },
-};
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: ['external', 'internal'],
+        pathGroups: [
+          {
+            pattern: 'react*',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'next/**',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@pinit/**',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@/**',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+  },
+}

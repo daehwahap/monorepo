a4dfedb4f9051b365e62ba9dff0f96666a4dced6
yoon-junseo <ysj6844@naver.com>
2024-06-01 16:06:22 +0900

    refactor: app에 eslint import/order 적용

--- apps/app/.eslintrc.js
+++ apps/app/.eslintrc.js
@@ -4,4 +4,40 @@ module.exports = {
   parserOptions: {
     project: true,
   },
-};
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: ['external', 'internal'],
+        pathGroups: [
+          {
+            pattern: 'react*',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'react-native/**',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@pinit/**',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@/**',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: [],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+  },
+}

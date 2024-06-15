34fe422ed51218e4ee32b3d334d2e7f9a056a402
11t518s <11t518s20161759@gmail.com>
2024-06-16 00:25:28 +0900

    fix: 빌드오류 수정

--- apps/app/src/screens/BottomTab/TabBar.tsx
+++ apps/app/src/screens/BottomTab/TabBar.tsx
@@ -1,21 +1,17 @@
 import { BottomTabBarProps } from '@react-navigation/bottom-tabs'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { useAppRouter } from '../../shared/hooks/useAppRouter'
-import { AllScreenType } from '../type'
 import { BOTTOM_TAP_ROUTE_TYPE } from './type'
 
 const TabBar = (props: BottomTabBarProps) => {
-  const { bottom } = useSafeAreaInsets()
-
   const { navigate } = useAppRouter()
 
   const handlePress = (routeName: keyof BOTTOM_TAP_ROUTE_TYPE) => {
-    return navigate('BottomTab', { screen: routeName, params: {} })
+    return navigate('BottomTab', { screen: routeName })
   }
 
   return (
-    <View style={StyleSheet.flatten([styles.container, { paddingBottom: bottom || 32 }])}>
+    <View style={StyleSheet.flatten([styles.container, { paddingBottom: 32 }])}>
       {props.state.routes.map((route, index) => {
         const isFocused = props.state.index === index
 
